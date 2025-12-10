760c2f9144423598ec470709093336bab9cb06c3
1114nikhil <nikhiltk1114@gmail.com>
2025-12-10 13:58:01 +0530

    update in portfolio

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,29 @@
+import { navLinks,navIcons } from '#constants'
 import React from 'react'
+import dayjs from 'dayjs'
 
 const Navbar = () => {
   return (
     <nav>
         <div>
-            <img src="/images/logo.svg"/>
+            <img src="/images/logo.svg" alt='logo'/>
+            <p className='font-bold'>Nikhil Portfolio</p>
+            <ul>
+              {navLinks.map(({id,name})=>(
+                <li key={id}>
+                  <p>{name}</p>
+                </li>
+              ))}
+            </ul>
+        </div>
+        <div>
+          <ul>
+            {navIcons.map(({id,img})=>(
+              <li key={id}>
+                <img src={img} className='icon-hover' alt={`icon-${id}`}/>
+              </li>))}
+          </ul>
+          <time>{dayjs().format('ddd, MMM D h:mm A')}</time>
         </div>
     </nav>
   )
---
+++ src/components/Welcome.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Welcome = () => {
+  return (
+    <div>
+      Welcome to Nikhil's Portfolio!
+    </div>
+  )
+}
+
+export default Welcome
